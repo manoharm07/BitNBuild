8cdfe43b6815177ef228f3e91e1bd3d5d5c366cf
manoharm07 <manohar200407@gmail.com>
2024-09-28 14:43:19 +0530

    added headline

--- app/src/main/java/com/example/wisewear/ui/theme/UserInfo/profile.kt
+++ app/src/main/java/com/example/wisewear/ui/theme/UserInfo/profile.kt
@@ -17,11 +17,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.wisewear.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun userProfile() {
     // State variables to store user inputs
@@ -33,6 +37,7 @@ fun userProfile() {
     var expanded by remember { mutableStateOf(false) }
     val genderOptions = listOf("Male", "Female", "Other")
 
+
     // Column to arrange elements vertically
     Column(
         modifier = Modifier
@@ -41,6 +46,20 @@ fun userProfile() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize()
+        ) {
+            Text(
+                text = "Create your profile",
+                color = Color(0xFF3D5AFE),
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
         // Profile Image (placeholder for now)
         Box(
             modifier = Modifier.size(128.dp),
@@ -53,7 +72,7 @@ fun userProfile() {
                 modifier = Modifier
                     .size(128.dp)
                     .clip(CircleShape)
-                    .border(2.dp, Color.Gray, CircleShape)
+                    .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
             // Icon to add an image (overlay)
             Icon(
@@ -61,7 +80,7 @@ fun userProfile() {
                 contentDescription = "Add Image",
                 modifier = Modifier
                     .size(32.dp)
-                    .background(Color.White, CircleShape) // Add background to make it stand out
+                    .background(MaterialTheme.colorScheme.onSurface, CircleShape) // Add background to make it stand out
                     .padding(4.dp)
                     .clip(CircleShape)
                     .clickable {
@@ -76,7 +95,11 @@ fun userProfile() {
         OutlinedTextField(
             value = name,
             onValueChange = { name = it },
-            label = { Text("Name") },
+            label = { Text("Name", color = MaterialTheme.colorScheme.onSurface) },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
+            ),
             modifier = Modifier.fillMaxWidth()
         )
 
@@ -86,8 +109,12 @@ fun userProfile() {
         OutlinedTextField(
             value = age,
             onValueChange = { age = it },
-            label = { Text("Age") },
+            label = { Text("Age", color = MaterialTheme.colorScheme.onSurface) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
+            ),
             modifier = Modifier.fillMaxWidth()
         )
 
@@ -97,8 +124,12 @@ fun userProfile() {
         OutlinedTextField(
             value = email,
             onValueChange = { email = it },
-            label = { Text("Email ID") },
+            label = { Text("Email ID", color = MaterialTheme.colorScheme.onSurface) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
+            ),
             modifier = Modifier.fillMaxWidth()
         )
 
@@ -108,11 +139,9 @@ fun userProfile() {
         Box { // Wrapping the DropdownMenu inside a Box
             OutlinedTextField(
                 value = gender,
-                onValueChange = { gender = it}, // No need to handle text change
+                onValueChange = { gender = it }, // No need to handle text change
                 readOnly = true, // Makes it read-only for selection only
-                label = { Text("Gender") },
-                modifier = Modifier.fillMaxWidth(),
-
+                label = { Text("Gender", color = MaterialTheme.colorScheme.onSurface) },
                 trailingIcon = {
                     Icon(
                         imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
@@ -121,16 +150,18 @@ fun userProfile() {
                             expanded = true
                         }
                     )
-                }
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
+                ),
+                modifier = Modifier.fillMaxWidth()
             )
 
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false }, // Close dropdown when user clicks outside
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-
-
+                modifier = Modifier.align(Alignment.BottomCenter)
             ) {
                 genderOptions.forEach { option ->
                     DropdownMenuItem(
@@ -147,15 +178,22 @@ fun userProfile() {
         Spacer(modifier = Modifier.height(16.dp))
 
         // Submit Button
-        Button(onClick = {
-            // Handle the submit action here
-        }) {
+        Button(
+            onClick = {
+                // Handle the submit action here
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            ),
+
+        ) {
             Text(text = "Submit")
         }
     }
 }
 
-@Preview(showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun User() {
     userProfile()
